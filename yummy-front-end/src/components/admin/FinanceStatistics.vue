e7585b323c4f98d52f298a0e1126bfb7cfeb7439
Sephidator <Sephidator@gmail.com>
2019-06-09 19:35:25 +0800

    前端代码写完了hahah

--- yummy-front-end/src/components/admin/FinanceStatistics.vue
+++ yummy-front-end/src/components/admin/FinanceStatistics.vue
@@ -1,76 +1,175 @@
 <template>
   <div class="container">
-    <div ref="FinanceStatistics" class="line-chart"></div>
+    <div ref="FinanceStatistics" class="bubble-chart"></div>
   </div>
 </template>
 
 <script>
-import { OK } from '@/utils/status/HttpStatus';
+// import { OK } from '@/utils/status/HttpStatus';
 import { mapGetters } from 'vuex';
 
 export default {
   name: 'FinanceStatistics',
-  data () {
-    return {
-    };
-  },
   computed: {
-    ...mapGetters(['baseUrl'])
+    ...mapGetters(['baseUrl', 'colorBoard'])
   },
   mounted () {
-    this.setData();
+    this.setData('week');
   },
   methods: {
-    setData () {
+    handleClick (item) {
+      this.setData(item.key);
+    },
+    setData (key) {
+      // let myChart = this.$echarts.init(this.$refs.MemberStatistics);
+      // this.$http({
+      //   url: `${this.baseUrl}/admin/getMemberStatistics`,
+      //   method: 'GET'
+      // }).then((response) => {
+      //   const code = response.data.code;
+      //   if (code === OK) {
+      //     myChart.setOption(this.getOption(response.data.data, true));
+      //   } else {
+      //     this.$message.error(response.data.msg);
+      //   }
+      // });
       let myChart = this.$echarts.init(this.$refs.FinanceStatistics);
-      this.$http({
-        url: `${this.baseUrl}/admin/getFinanceStatistics`,
-        method: 'GET'
-      }).then((response) => {
-        const code = response.data.code;
-
-        if (code === OK) {
-          const data = response.data.data;
-          const dateList = data.map((item) => { return item.name; });
-          const valueList = data.map((item) => { return item.value; });
-
-          const option = {
-            tooltip: {
-              trigger: 'axis',
-              formatter: '{b} <br/>收入金额: {c}元'
+      myChart.setOption(this.getOption());
+    },
+    getOption () {
+      return {
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          }
+        },
+        toolbox: {
+          right: 20,
+          feature: {
+            saveAsImage: {
+              show: true
+            }
+          }
+        },
+        legend: {
+          data: [
+            '日交易额', '平台收入', '净利润', '分成比例', '毛利率'
+          ]
+        },
+        xAxis: [
+          {
+            type: 'category',
+            axisTick: {
+              alignWithLabel: true
             },
-            xAxis: {
-              type: 'category',
-              data: dateList
+            data: [
+              '2019-06-01', '2019-06-02', '2019-06-03', '2019-06-04',
+              '2019-06-05', '2019-06-06', '2019-06-07', '2019-06-08',
+              '2019-06-09', '2019-06-10', '2019-06-11', '2019-06-12'
+            ]
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value',
+            name: '营业额',
+            min: 0,
+            max: 1000,
+            position: 'left',
+            axisLabel: {
+              formatter: '{value} 元'
+            }
+          },
+          {
+            type: 'value',
+            name: '营业额增长率',
+            min: -100,
+            max: 100,
+            position: 'right',
+            axisLabel: {
+              formatter: '{value} %'
             },
-            yAxis: {
-              type: 'value'
+            splitLine: {
+              lineStyle: {
+                type: 'dotted'
+              }
             },
-            series: [{
-              data: valueList,
-              type: 'line'
-            }]
-          };
-          myChart.setOption(option);
-        } else {
-          this.$message.error(response.data.msg);
-        }
-      });
+            axisLine: {
+              lineStyle: {
+                color: '#888',
+                type: 'dashed'
+              }
+            }
+          }
+        ],
+        series: [
+          {
+            name: '日交易额',
+            type: 'bar',
+            data: [192, 374, 579, 734, 683, 498, 514, 374, 579, 734, 683, 498],
+            itemStyle: {
+              color: '#FF7000'
+            }
+          },
+          {
+            name: '平台收入',
+            type: 'bar',
+            data: [374, 683, 498, 514, 702, 698, 768, 374, 683, 498, 514, 702],
+            itemStyle: {
+              color: '#FFCC00'
+            }
+          },
+          {
+            name: '净利润',
+            type: 'bar',
+            data: [192, 374, 579, 498, 514, 822, 768, 579, 498, 514, 822, 768],
+            itemStyle: {
+              color: '#9ACD32'
+            }
+          },
+          {
+            name: '分成比例',
+            type: 'line',
+            itemStyle: {
+              color: '#4D80E6',
+              opacity: 1
+            },
+            lineStyle: {
+              width: 5
+            },
+            yAxisIndex: 1,
+            data: [87, 79, 80, 72, 86, 75, 80, 80, 92, 95, 87, 93]
+          },
+          {
+            name: '毛利率',
+            type: 'line',
+            itemStyle: {
+              color: '#0000CD',
+              opacity: 1
+            },
+            lineStyle: {
+              width: 5
+            },
+            yAxisIndex: 1,
+            data: [90, 88, 82, 84, 78, 81, 90, 85, 92, 87, 79, 83]
+          }
+        ]
+      };
     }
   }
 };
 </script>
 
 <style scoped>
-  .line-chart {
-    height: 600px;
-    width: 1200px;
+  .bubble-chart {
+    height: 700px;
+    width: 1280px;
     display: inline-block;
   }
   .container {
-    height: 750px;
-    width: 1200px;
-    min-width: 1200px;
-    padding: 20px 0;
+    height: 700px;
+    width: 100%;
+    min-width: 1280px;
   }
 </style>
